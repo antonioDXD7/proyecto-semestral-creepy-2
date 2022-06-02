74504b88b78df091ad68c1a40e4df0664117df5d
antonioDXD7 <101341778+antonioDXD7@users.noreply.github.com>
2022-06-01 22:34:34 -0400

    Add files via upload

---
+++ proyecto-semestral-creepy-2-final/paginas/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path('', inicio, name="inicio"),
+    path('ajustedatos/', ajustedatos, name="ajustedatos"),
+    path('ajustecontrasenia/', ajustecontrasenia, name="ajustecontrasenia"),
+    path('historias/<slug:idcreepypasta>', historias, name="historias"),
+    path('informacion/', informacion, name="informacion"),
+    path('logueate/', logueate, name="logueate"),
+    path('listadom/', listadom, name="listadom"),
+    path('registro/', registro, name="registro"),
+    path('subircreepy/', subircreepy, name="subircreepy"),
+    path('tushistorias/', tushistorias, name="tushistorias"),
+    path('vermensajesadmi/', vermensajesadmi, name="vermensajesadmi"),
+    path('enviarmensajeadmi/', enviarmensajeadmi, name="enviarmensajeadmi"),
+    path('registraM/', registraM, name="registraM"),
+    path('registraMEN/', registraMEN, name="registraMEN"),
+    path('eliminarcreepy/<codigos>', eliminarcreepy, name="eliminarcreepy"),
+    path('borrarmensajes/<id>', borrarmensajes, name="borrarmensajes"),
+
+    path('usuario/<usuario>', cargar_usuario, name='cargar_usuario'),
+    path('megusta/<idcreepypasta>', añadir_megusta, name='añadir_megusta'),
+    path('agregar_comentario/<idcreepypasta>',
+         agregar_comentario, name='agregar_comentario')
+
+]
