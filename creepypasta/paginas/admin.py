c8be238cc7376a81746654767e858276f82d95a9
antonioDXD7 <101341778+antonioDXD7@users.noreply.github.com>
2022-06-01 22:32:49 -0400

    Delete creepypasta directory

--- creepypasta/paginas/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import genero, creepypasta ,mensajesadmi2
-# Register your models here.
-admin.site.register(genero)
-admin.site.register(creepypasta)
-admin.site.register(mensajesadmi2)
