656440b22d54029521c7d0bedacf1cddcb8c58d5
antonioDXD7 <101341778+antonioDXD7@users.noreply.github.com>
2022-06-07 10:15:01 -0400

    Add files via upload

---
+++ proyecto-semestral-creepy-2-final v7.1/paginas/urls.py
@@ -0,0 +1,35 @@
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path('', inicio, name="inicio"),
+    path('historias/<slug:idcreepypasta>', historias, name="historias"),
+    path('informacion/', informacion, name="informacion"),
+    path('logueate/', logueate, name="logueate"),
+    path('listadom/', listadom, name="listadom"),
+    path('registro/', registro, name="registro"),
+    path('subircreepy/', subircreepy, name="subircreepy"),
+    path('tushistorias/', tushistorias, name="tushistorias"),
+    path('vermensajesadmi/', vermensajesadmi, name="vermensajesadmi"),
+    path('enviarmensajeadmi/', enviarmensajeadmi, name="enviarmensajeadmi"),
+    path('registraM/', registraM, name="registraM"),
+    path('registraMEN/', registraMEN, name="registraMEN"),
+    path('eliminarcreepy/<codigos>', eliminarcreepy, name="eliminarcreepy"),
+    path('eliminarcreepy1/<codigos1>', eliminarcreepy1, name="eliminarcreepy1"),
+    path('borrarmensajes/<id>', borrarmensajes, name="borrarmensajes"),
+    path('borrarusuario/<ide>', borrarusuario, name="borrarusuario"),
+    path('borrarcomentarios/<idees>', borrarcomentarios, name="borrarcomentarios"),
+
+    path('usuario/<usuario>', cargar_usuario, name='cargar_usuario'),
+    path('megusta/<idcreepypasta>', añadir_megusta, name='añadir_megusta'),
+    path('agregar_comentario/<idcreepypasta>', agregar_comentario, name='agregar_comentario'),
+    path('verusuariosadmi/', verusuariosadmi, name="verusuariosadmi"),
+    path('ajustedatos/<ides>', ajustedatos, name="ajustedatos"),
+    path('ajustescomentarios/<ideees>', ajustescomentarios, name="ajustescomentarios"),
+
+
+     
+
+
+]
---
+++ proyecto-semestral-creepy-2-final v7.1/usuarios/forms.py
@@ -0,0 +1,25 @@
+from dataclasses import fields
+from django import forms
+from usuarios.models import Usuario
+from paginas.models import Comentario
+#from paginas.models import Usuario
+
+class usuariosForm(forms.ModelForm):
+
+    primer_nombre = forms.CharField(min_length=3,max_length=9)
+    apellido = forms.CharField(min_length=3,max_length=9)
+    correo = forms.EmailField(min_length=14,max_length=30)
+
+    class Meta:
+        model = Usuario
+        fields = ["primer_nombre","apellido","correo"]
+    
+
+class comentarioForm(forms.ModelForm):
+
+
+    class Meta:
+        model = Comentario
+        fields = ["cuerpo"]
+
+
